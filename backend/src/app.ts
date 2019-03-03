62c5d8afaaf1da2144f4f509ceaf369fc7bf3430
odan <odan3240@gmail.com>
2019-03-03 22:55:23 +0900

    nuxt ssr

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,15 @@
+import path from "path"
 import express from "express"
+const { Nuxt } = require('nuxt')
+
+const FRONTEND_DIR = path.join(__dirname, "../../frontend")
+const port = process.env.PORT || 3000
+
+const config = require(path.join(FRONTEND_DIR, './nuxt.config.ts'))
+config.buildDir = path.join(FRONTEND_DIR, config.buildDir || '.nuxt')
+// why
+config.dev = false
+const nuxt = new Nuxt(config)
 
 const app = express()
 
@@ -6,4 +17,7 @@ app.get('/hello', function(_, res) {
     res.send('hello');
 });
 
-app.listen(3000);
+app.use(nuxt.render)
+
+console.log(`start listen: ${port}`)
+app.listen(port);
